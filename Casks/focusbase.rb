130392001e54b0daf066938b9f5122936b8856e5
Andrew Schreiber <andrew.schreiber1@gmail.com>
2026-09-03 02:30:36 -0400

    Update to v2.9.2

--- Casks/focusbase.rb
+++ Casks/focusbase.rb
@@ -1,6 +1,6 @@
 cask "focusbase" do
-  version "2.9.1"
-  sha256 "0c1ee0e94c2ae5bd17ba0c8a0de0d681c60a6f4716ccf53a5d55d5290bb9f8b0"
+  version "2.9.2"
+  sha256 "9e5b38720429bd94ed69ad5ce5af467423377722dfff1b2d552cf9686e8b4874"
 
   url "https://github.com/andrewschreiber/dozy-releases/releases/download/v#{version}/Focusbase-#{version}-arm64.dmg",
       verified: "github.com/andrewschreiber/dozy-releases/"
